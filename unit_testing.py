9fed67d010906671ba2fc6e2d86f43d2b4f4ca43
Sissi Feng <38707787+SissiFeng@users.noreply.github.com>
2024-09-21 08:44:54 -0400

    Update and rename bayesian_optimization.py to unit_testing.py

---
+++ unit_testing.py
@@ -0,0 +1,31 @@
+class ColorMixer:
+    @staticmethod
+    def mix_colors(color1, color2):
+        """
+        Mix two colors and return the result.
+        Colors should be in the format (R, G, B) where R, G, and B are integers from 0 to 255.
+        """
+        # TODO: Implement color mixing logic
+        pass
+
+    @staticmethod
+    def lighten_color(color, amount):
+        """
+        Lighten a color by a certain amount.
+        Color should be in the format (R, G, B) where R, G, and B are integers from 0 to 255.
+        Amount should be a float between 0 and 1.
+        """
+        # TODO: Implement color lightening logic
+        pass
+
+    @staticmethod
+    def darken_color(color, amount):
+        """
+        Darken a color by a certain amount.
+        Color should be in the format (R, G, B) where R, G, and B are integers from 0 to 255.
+        Amount should be a float between 0 and 1.
+        """
+        # TODO: Implement color darkening logic
+        pass
+
+# You can add more methods or helper functions as needed
